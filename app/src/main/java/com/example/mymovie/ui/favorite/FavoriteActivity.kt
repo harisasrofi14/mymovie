ead79577a66df8af4d0942784e05514c3878e7f4
haris <harisasrofi14@gmail.com>
2021-03-09 21:02:00 +0700

    Changes : - Create Activities - Create OptionMenu

--- app/src/main/java/com/example/mymovie/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/example/mymovie/ui/favorite/FavoriteActivity.kt
@@ -1,11 +1,12 @@
-package com.example.mymovie
+package com.example.mymovie.ui.favorite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mymovie.R
 
-class MainActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_favorite)
     }
 }
